d5df185521f82457b37ec7f06c0baec4c362155e
Shriram <vshriram93@gmail.com>
2014-12-26 23:41:18 +0530

    started with code refactor for landing page and first page php file

--- login.php
+++ login.php
@@ -1,67 +1,80 @@
 
 <html>
 <head>
-<script type="text/javascript" src="jquery.js"></script>
-<link rel="stylesheet" href="magnific-popup/magnific-popup.css"> 
-<link rel="stylesheet" type="text/css" href="lightbox.css">
-<!-- Magnific Popup core JS file -->
-<script src="jquery.js"></script>
+  <script type="text/javascript" src="jquery.js"></script>
+  <link rel="stylesheet" href="magnific-popup/magnific-popup.css"> 
+  <link rel="stylesheet" type="text/css" href="lightbox.css">
+  <!-- Magnific Popup core JS file -->
+  <script src="jquery.js"></script>
 
-<link rel="stylesheet" type="text/css" href="layout.css">
+  <link rel="stylesheet" type="text/css" href="layout.css">
 </head>
 
 <body>
-<center>
-	<h1>BEER Factory</h1>
-</center>
-
-
-<div class="box effect">
-	<div class="login">
-        
-        <br><br>
-        <p><center><h3>LOGIN WITH YOUR ID</h3></center></p> 
-        <center><h3>ID:       <input type="ID" id="loginid"></h3>
-        </center>
-        <center>
-        	<button onclick="check($('#loginid').val())">LOGIN</button>
-        </center>
-	</div>
-
-
-
-</div>
-<form method="post" id="f1">
-<input type="hidden" name="name" id="1">
-</form>
-<!--form action="stage2.php" method="post" id="f2">
-<input type="hidden" name="name" id="2">
-</form>
-<form action="stage3.php" method="post" id="f3">
-<input type="hidden" name="name" id="3">
-</form>
-<form action="stage4.php" method="post" id="f4">
-<input type="hidden" name="name" id="4">
-</form-->
+  <center>
+    <h1>BEER Factory</h1>
+  </center>
+  <div class="box effect">
+    <div class="login">
+      <br><br>
+      <p><center><h3>LOGIN WITH YOUR ID</h3></center></p> 
+      <center>
+        <h3>ID: <input type="ID" id="loginid"></h3>
+      </center>
+      <center>
+        <button onclick="check($('#loginid').val())">LOGIN</button>
+      </center>
+    </div>
+  </div>
+  <form method="post" id="f1">
+    <input type="hidden" name="name" id="1">
+  </form>
+  <!--
+  form action="stage2.php" method="post" id="f2">
+  <input type="hidden" name="name" id="2">
+  </form>
+  <form action="stage3.php" method="post" id="f3">
+  <input type="hidden" name="name" id="3">
+  </form>
+  <form action="stage4.php" method="post" id="f4">
+  <input type="hidden" name="name" id="4">
+  </form
+  -->
+  <script>
+    function check(s) { 
+      document.getElementById('1').value=s;
+      $.ajax({
+        type:"post",
+        url:"check.php",
+        data: {s:s},
+        success:function(data){
+          if(data==1){
+            var frm=document.getElementById('f1');
+            document.getElementById('f1').action="stage1.php";
+            frm.submit();
+          }
+          else if (data==2) {
+            var frm=document.getElementById('f1');
+            document.getElementById('f1').action="stage2.php";
+            frm.submit();
+          }
+          else if (data==3) {
+            var frm=document.getElementById('f1');
+            document.getElementById('f1').action="stage3.php";
+            frm.submit();
+          }
+          else if (data==4) {
+            var frm=document.getElementById('f1');
+            document.getElementById('f1').action="stage4.php";
+            frm.submit();
+          }
+          else {
+            alert("YOUR ID DOES NOT EXIST. ENTER A VALID ID")
+          }
+        }
+      });
+    }
+  </script>
 </body>
 </html>
 
-<script>
-	function check(s) { 
-                document.getElementById('1').value=s;
-                
-                $.ajax({
-                            type:"post",
-                            url:"check.php",
-                            data: {s:s},
-                            success:function(data){
-                              if(data==1){var frm=document.getElementById('f1');document.getElementById('f1').action="stage1.php";frm.submit();}
-                              else if (data==2) {var frm=document.getElementById('f1');document.getElementById('f1').action="stage2.php";frm.submit();}
-                              else if (data==3) {var frm=document.getElementById('f1');document.getElementById('f1').action="stage3.php";frm.submit();}
-                              else if (data==4) {var frm=document.getElementById('f1');document.getElementById('f1').action="stage4.php";frm.submit();}
-                              else {alert("YOUR ID DOES NOT EXIST. ENTER A VALID ID")};
-                            }
-                               });
-
-	}
-</script>
--- retailerStage.php
+++ retailerStage.php
@@ -5,172 +5,141 @@ $id=$_POST['name'];
 //$_SESSION['id']=$_POST['name'];
 $result = mysqli_query($conn,"SELECT * FROM team WHERE Player1=$id");
 
-while($row = mysqli_fetch_array($result))
-  {
+while($row = mysqli_fetch_array($result)) {
   $curr=$row['Player1']; $next=$row['Player2']; 
-  }
-
-
+}
 echo "<script>var curr=$curr;var next=$next;</script>";
 ?>
+
 <html>
 <head>
-<script type="text/javascript" src="jquery.js"></script>
-<script src="retailer.js"></script>
-<script src="stage.js"></script>
-<script src="transaction.js"></script>
-<link rel="stylesheet" href="magnific-popup/magnific-popup.css"> 
-<link rel="stylesheet" type="text/css" href="lightbox.css">
-<!-- Magnific Popup core JS file -->
-<script src="magnific-popup/jquery.magnific-popup.js"></script>
+  <script type="text/javascript" src="jquery.js"></script>
+  <script src="retailer.js"></script>
+  <script src="stage.js"></script>
+  <script src="transaction.js"></script>
+  <link rel="stylesheet" href="magnific-popup/magnific-popup.css"> 
+  <link rel="stylesheet" type="text/css" href="lightbox.css">
+  <!-- Magnific Popup core JS file -->
+  <script src="magnific-popup/jquery.magnific-popup.js"></script>
 
-<link rel="stylesheet" type="text/css" href="layout.css">
+  <link rel="stylesheet" type="text/css" href="layout.css">
 </head>
-
 <body>
-<center>
-	<h1>BEER Factory</h1>
-</center>
+  <center>
+    <h1>BEER Factory</h1>
+  </center>
+  <div class="cost">
+    <p id="Icost">Inventory cost:<span>200</span></p>
+    <p id="Bcost">Backlog cost:<span>300</span></p>
+  </div>
+  <div class="box effect">
+    <p id="Round">Round No:<span>1</span></p>
+    <div class="innerBox1">
+      <center>
+        <h2>CUSTOMER</h2>
+      </center>
+    </div>
+    <div class="innerBox2">
+      <center>
+        <h2>RETAILER</h2>
+      </center>
+      <div id="orderdiv" >
+        <p>ORDER:</p>
+        <input type="number" id="order" class="field" name="order" min="1">
+        <input type="button" id="orderbutton" class="button" onclick="order()" value="ok">
+      </div>
+      <br>
+      <div id="supplydiv" >
+        <p>SUPPLY:</p>
+        <input type="number" id="supply" class="field" name="supply" min="1">
+        <input type="button" id="supplybutton" class="button" onclick="supply()" value="ok">
+      </div>
+      <br>
+      <div class="details">
+        <p>Pending order:<span id="Porder">20</span></p><br>
+        <p>Inventory:<span id="Inv">30</span></p>
+      </div>
+    </div>
+    <div class="innerBox3">
+      <center>
+        <h2>WHOLESALER</h2>
+      </center>
+    </div>
+  </div>
+  <button onclick="show()">View Transactions</button>
+  <script type="text/javascript">
+    var flag=0,backup=1;
+    function supplybackup() {
+     $.ajax({
+      type:"post",
+      url:"player.php",
+      data: {curr:curr,backup:'1',inventory:$('#Inv').text(),porder:$('#Porder').text()},
+      success:function(data){
+        console.log(data);
+      }
+    });
+   }
 
-<div class="cost">
-<p id="Icost">Inventory cost:<span>200</span></p>
-	<p id="Bcost">Backlog cost:<span>300</span></p>
-	</div>
-	
-	
-	
+   function supply() {  
+     flag=1;    
+     if(($('#supply').val()<=parseInt($('#Inv').text())) && 
+      ($('#supply').val()<=parseInt($('#Porder').text()))) {
+      $('#Porder').text(parseInt($('#Porder').text())-parseInt($('#supply').val()));
+      $('#Inv').text(parseInt($('#Inv').text())-parseInt($('#supply').val()));
+      alert("supplied"); 
+      supplybackup();
+     }
+     else {
+      alert("Invalid Transaction");
+     }
+   }
 
-<div class="box effect">
-	<p id="Round">Round No:<span>1</span></p>
-<div class="innerBox1">
-<center>
-	<h2>CUSTOMER</h2>
-</center>
-</div>
-<div class="innerBox2">
-<center>
-	<h2>RETAILER</h2>
-</center>
-<div id="orderdiv" >
-	<p>ORDER:</p>
-	<input type="number" id="order" class="field" name="order" min="1">
-	<input type="button" id="orderbutton" class="button" onclick="order()" value="ok">
-</div>
-<br>
-<div id="supplydiv" >
-	<p>SUPPLY:</p>
-	<input type="number" id="supply" class="field" name="supply" min="1">
-	<input type="button" id="supplybutton" class="button" onclick="supply()" value="ok">
-
-</div>
-<br>
-<div class="details">
-	<p>Pending order:<span id="Porder">20</span></p><br>
-	<p>Inventory:<span id="Inv">30</span></p>
-	</div>
-</div>
-<div class="innerBox3">
-<center>
-	<h2>WHOLESALER</h2>
-</center>
-</div>
-</div>
-<button onclick="show()">View Transactions</button>
+   function order() {  
+     flag=2;               
+     $.ajax({
+      type:"post",
+      url:"player.php",
+      data: {id:curr,order:$('#order').val(),
+      supply:'0',
+      round:$('#Round>span').text(),
+      f:flag},
+      success:function(data) {
+        alert("order has been placed");
+      }
+    });
+   }
+   setInterval(function(){
+    $.ajax({
+      type:"post",
+      url:"player.php",
+      data: {curr:curr,next:next},
+      success:function(data){
+        if(data!='None') {
+          setTimeout(function(){ console.log("hey".data);alert(data);
+           var res = data.split(" ");
+           $('#Inv').text(parseInt($('#Inv').text())+parseInt(res[3]));}, 5000);
+          supplybackup();
+        } 
+      }
+    });
+   }, 1000);         
+   window.onbeforeunload = function() {
+    return "Do not Refresh the game. You will lose all data."; }
+    window.onload=function(){
+      backup=1;
+      $.ajax({
+        type:"post",
+        url:"backup.php",
+        data: {backup:'1',curr:curr},
+        success:function(data){
+          var res = data.split(" ");
+          $('#Inv').text(parseInt(res[0]));
+          $('#Porder').text(parseInt(res[1]));
+          $('#Icost>span').text(parseInt(res[2]));
+          $('#Bcost>span').text(parseInt(res[3]));
+        }
+      });
+    }
+  </script>
 </body>
-<script type="text/javascript">
-
-
-
-  var flag=0,backup=1;
-
-  function supplybackup()
-       {
-                         $.ajax({
-                            type:"post",
-                            url:"player.php",
-                            data: {curr:curr,backup:'1',inventory:$('#Inv').text(),porder:$('#Porder').text()},
-                            success:function(data){
-                              console.log(data);
-                            }
-                               });
-       }
-	
-
-   function supply(){  
-                         flag=1;    
-						if(($('#supply').val()<=parseInt($('#Inv').text())) && ($('#supply').val()<=parseInt($('#Porder').text()))){
-							$('#Porder').text(parseInt($('#Porder').text())-parseInt($('#supply').val()));
-							$('#Inv').text(parseInt($('#Inv').text())-parseInt($('#supply').val()));
-							alert("supplied"); 
-							supplybackup();
-
-            }
-							
-						else{
-						alert("Invalid Transaction");}
-                         
-                      }
-
-
-
-              function order(){  
-                         flag=2;               
-                         $.ajax({
-                            type:"post",
-                            url:"player.php",
-                            data: {id:curr,order:$('#order').val(),
-				   supply:'0',
-                                   round:$('#Round>span').text(),
-                                   f:flag},
-                            success:function(data){
-                                alert("order has been placed");
-                             
-
-                          }
-                             });
-                             
-                      }
-    
-
-
-  setInterval(function(){$.ajax({
-                            type:"post",
-                            url:"player.php",
-                            data: {curr:curr,next:next},
-                            success:function(data){
-                                if(data!='None')
-                                 {setTimeout(function(){ console.log("hey".data);alert(data);
-								 var res = data.split(" ");
-								 //alert(res[3]);
-								 $('#Inv').text(parseInt($('#Inv').text())+parseInt(res[3]));}, 5000);
-								supplybackup();} 
-                             
-
-                                 }
-                             });}, 1000);         
-
-         
-
-
-          window.onbeforeunload = function() {
-        return "Do not Refresh the game. You will lose all data."; }
-
-        window.onload=function(){
-          backup=1;
-                    $.ajax({
-                            type:"post",
-                            url:"backup.php",
-                            data: {backup:'1',curr:curr},
-                            success:function(data){
-							var res = data.split(" ");
-							$('#Inv').text(parseInt(res[0]));
-							$('#Porder').text(parseInt(res[1]));
-							$('#Icost>span').text(parseInt(res[2]));
-							$('#Bcost>span').text(parseInt(res[3]));
-                              
-                            }
-                               });
-                  }
-         
-</script>
 </html>
